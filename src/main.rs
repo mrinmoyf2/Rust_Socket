df26d1f5c84a32a846881647842c926354c67f07
mrinmoy <priyadarsanf2@gmail.com>
2023-12-28 14:02:19 +0530

    Error: info_make_service()

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
+use axum::routing::get;
+use tracing::info;
+use tracing_subscriber::FmtSubscriber;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing::subscriber::set_global_default(FmtSubscriber::default())?;
+
+    let app = axum::Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    info!("Starting server");
+
+    axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
+        .serve(app.info_make_service())
+        .await?;
+
     Ok(())
 }
